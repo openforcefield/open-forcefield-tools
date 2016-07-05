40f373a0a25945900c33c159dadb46d35c9be87a
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-07-04 22:32:40 -0400

    More basic sketching.

--- openforcefield/propertyestimator.py
+++ openforcefield/propertyestimator.py
@@ -69,6 +69,13 @@ class ComputedProperty(object):
     def __init__(self):
         self.simulations = set()
 
+class ComputedPropertySet(list):
+    """Set of computed physical properties.
+
+    """
+    def __init__(self):
+        pass
+
 #=============================================================================================
 # PROPERTY ESTIMATOR
 #=============================================================================================
@@ -92,21 +99,45 @@ class PropertyEstimator(object):
         """
         pass
 
-    def computeProperties(self, dataset, parameter_sets):
+    def computeProperties(self, dataset, parametersets, target_relative_uncertainty=0.1):
         """Compute physical properties for the specified dataset given one or more parameter sets.
 
         Parameters
         ----------
         dataset : PhysicalPropertyDataset
             The dataset for which physical properties are to be computed.
-        parameter_sets : ParameterSet or iterable of Parameterset
+        parametersets : ParameterSet or iterable of Parameterset
             Parameter set(s) for which physical properties are to be computed.
+        target_uncertainty : float, optional, default=0.1
+            Target computational uncertainty in the computed property, relative to experimental uncertainty.
 
         Returns
         -------
-        properties : list of ComputedProperty
-            The computed
+        properties : ComputedPropertySet object or list of ComputedPropertySet objects
+            The computed physical properties.
 
         """
-        computed_properties =
-        return computed_properties
+        # Attempt to estimate all simulated properties by reweighting
+        simulations_to_run = list() # list of simulations that need to be rerun
+        computed_properties_sets = list()
+        for parameters in parameter_sets:
+            computed_property_set = ComputedPropertySet()
+            for measured_property in dataset:
+                # Estimate property via reweighting.
+                computed_property = self._estimateProperty(measured_property)
+                if (computed_property is None) or (computed_property.uncertainty > target_relative_uncertainty * measured_property.uncertainty):
+                    # Uncertainty threshold exceeded; queue for simulation
+                    simulation = Simulation(thermodynamic_state=measured_property.thermodynamic_state, composition=thermodynamic_state.composition)
+                    simulations_to_run.append( (simulation, measured_property, target_relative_uncertainty) )
+
+                computed_property = ComputedProperty()
+                computed_property_set.append(computed_property)
+            computed_properties_sets.append(computed_property_set)
+
+        # Run queued simulations
+        # TODO: Parallelize
+        for (simulation, target_property, target_uncertainty) in simulations_to_run:
+            simulation.run(target_property=target_property, target_uncertainty=target_uncertainty)
+
+        # Return computed physical property dataset(s)
+        return computed_properties_sets
