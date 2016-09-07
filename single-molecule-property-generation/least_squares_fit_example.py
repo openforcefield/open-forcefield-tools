094cadc97fcbc5d819ad5c914d7a471a86c75042
Bryce Manubay <bmanubay@laplatapeak.int.colorado.edu>
2016-09-07 15:22:26 -0600

    tors hist

--- single-molecule-property-generation/least_squares_fit_example.py
+++ single-molecule-property-generation/least_squares_fit_example.py
@@ -394,16 +394,14 @@ torsion = np.zeros([1,100],np.float64)
 
 torsion = torstimeser[0]
 
-step = 0.1
-bins = np.arange(np.around(np.min(torsion),decimals=1),np.around(np.max(torsion),decimals=1)+step,step)
+step = 0.05
+bins = np.arange(np.around(np.min(torsion),decimals=1),np.around(np.max(torsion),decimals=1),step)
 
 binplace = np.digitize(torsion, bins)
 
 print binplace
 
 likelihood = (np.bincount(binplace))/100.
-print bins
-print likelihood
 
 
 
@@ -419,7 +417,7 @@ plt.show()
 plt.savefig("some figure.png")
 
 plt.figure()
-plt.hist(torsion, bins)
+plt.hist(torsion, bins, normed=1)
 plt.ylabel('Likelihood that configuration is sampled')
 plt.xlabel('Torsion angle (radians)')
 plt.savefig('Torsion_likelihood.png')
