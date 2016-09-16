9e298a2f8605c1464349f3a836b963f54049adb5
Bryce Manubay <bmanubay@laplatapeak.int.colorado.edu>
2016-09-16 16:37:53 -0600

    Comparison of torsions by SMIRKS for r48 and r51

--- single-molecule-property-generation/least_squares_fit_example.py
+++ single-molecule-property-generation/least_squares_fit_example.py
@@ -394,10 +394,10 @@ data_r48, xyz_r48, time_r48 = readtraj([traj[0]])
 data_r51, xyz_r51, time_r51 = readtraj([traj[1]])
 #print len(xyz_r51)
 
-xyzn_r48 = unit.Quantity(xyz_r48[:9238], unit.angstroms)
-time_r48 = unit.Quantity(time_r48[:9238], unit.picoseconds)
-xyzn_r51 = unit.Quantity(xyz_r51[:], unit.angstroms)
-time_r51 = unit.Quantity(time_r51[:], unit.picoseconds)
+xyzn_r48 = unit.Quantity(xyz_r48[:], unit.angstroms)
+time_r48 = unit.Quantity(time_r48[:], unit.picoseconds)
+xyzn_r51 = unit.Quantity(xyz_r51[:9238], unit.angstroms)
+time_r51 = unit.Quantity(time_r51[:9238], unit.picoseconds)
 
 # Compute bond lengths and angles and return array of angles
 a = ComputeBondsAnglesTorsions(xyzn_r48,AtomDict_r48['Bond'],AtomDict_r48['Angle'],AtomDict_r48['Torsion'])
