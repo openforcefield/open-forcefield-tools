1cc62b02d64fcae7f646cc4602c036e1631b5047
Bryce Manubay <brma3379@login02.rc.colorado.edu>
2017-03-01 17:34:14 -0700

    torsion fit, make frequencies non-zero

--- single-molecule-property-generation/least_squares_fit_example.py
+++ single-molecule-property-generation/least_squares_fit_example.py
@@ -502,9 +502,10 @@ num_bins = 500
 
 plt.figure()
 (n1,bins1,patch1) = plt.hist(torsion_r48_a,num_bins,label='AlkEthOH_r48',color='green')
-for i in n1:
-	if i==0:
-           i+=5
+for i,j in enumerate(n1):
+	if j==0.:
+           n1[i]=5.
+print n1
 popt, pcov = sci.curve_fit(fourier, bins1[1:], n1, [1.0] * 7, bounds=(0,np.inf))
 plt.plot(bins1[1:],fourier(bins1[1:],popt[0],popt[1],popt[2],popt[3],popt[6]))
 plt.ylabel('Number of times configuration is sampled')
