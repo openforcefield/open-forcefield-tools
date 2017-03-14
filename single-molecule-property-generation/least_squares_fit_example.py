2416a4e26d46ceefa918bc0512db2c97cac83b36
Bryce Manubay <brma3379@login04.rc.colorado.edu>
2017-03-13 20:38:38 -0600

    Add phase as a parameter

--- single-molecule-property-generation/least_squares_fit_example.py
+++ single-molecule-property-generation/least_squares_fit_example.py
@@ -512,7 +512,7 @@ plt.figure()
 #	if j==0.:
 #           n1[i]=5.
 
-popt, pcov = sci.curve_fit(fourier, bins1[1:], n1, [1.0,0.0,1.0]*3, maxfev=100000)
+popt, pcov = sci.curve_fit(fourier, bins1[1:], n1, [1.0,1.0,1.0]*3, maxfev=100000)
 plt.plot(bins1[1:],fourier(bins1[1:],*popt),label='AlkEthOH_r48 fourier fit')
 plt.ylabel('Number of times configuration is sampled')
 plt.xlabel('Torsion angle (radians)')
